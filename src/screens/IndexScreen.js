f42316b660a107081d14bc2788569338e0b38c1a
Antonio Jack <ict2jack@gmail.com>
2019-08-27 23:28:06 +0700

    130. A Bit of Styling

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,25 +1,41 @@
 import React, { useContext } from 'react'
 import { StyleSheet, Text, View, Button, FlatList } from 'react-native'
 import { Context } from '../context/BlogContext'
+import { Feather } from '@expo/vector-icons'
 
 export default () => {
   const { state, addBlogPost } = useContext(Context)
 
   return (
-    <View style={styles.container}>
-      <Text>index screen</Text>
-      <Button title="Add" onPress={addBlogPost} />
+    <View>
+      <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={post => post.title}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
+        renderItem={({ item }) => (
+          <View style={styles.row}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Feather name="trash" style={styles.icon} />
+          </View>
+        )}
       />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    borderTopWidth: 1,
+    borderTopColor: 'gray',
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
   },
 })
