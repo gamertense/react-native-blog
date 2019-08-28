db46570abc8af4d24be4ce5aeb43410ff2d3cd29
Antonio Jack <ict2jack@gmail.com>
2019-08-28 10:54:10 +0700

    remove unused variable

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -11,7 +11,7 @@ import { Context } from '../context/BlogContext'
 import { Feather } from '@expo/vector-icons'
 
 const IndexScreen = ({ navigation }) => {
-  const { state, addBlogPost, deleteBlogPost } = useContext(Context)
+  const { state, deleteBlogPost } = useContext(Context)
 
   return (
     <View>
