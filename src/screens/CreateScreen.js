526100976fa9839852058121fae988054b1de10a
Antonio Jack <ict2jack@gmail.com>
2019-08-28 10:40:21 +0700

    displaying a form

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,17 +1,42 @@
-import React, { useContext } from 'react'
-import { StyleSheet, View, Text } from 'react-native'
-import { Context } from '../context/BlogContext'
+import React, { useState } from 'react'
+import { StyleSheet, View, Text, TextInput } from 'react-native'
+// import { Context } from '../context/BlogContext'
 
 export default ({ navigation }) => {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
   return (
-    <View style={styles.container}>
-      <Text>Create screen </Text>
+    <View>
+      <Text style={styles.label}>Enter Title </Text>
+      <TextInput
+        value={title}
+        onChangeText={text => setTitle(text)}
+        style={styles.input}
+      />
+      <Text style={styles.label}>Enter Content: </Text>
+      <TextInput
+        value={content}
+        onChangeText={text => setContent(text)}
+        style={styles.input}
+      />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  label: {
+    margin: 15,
+    fontSize: 18,
+    marginBottom: 15,
+  },
+  input: {
+    fontSize: 15,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 13,
+    padding: 5,
+    marginHorizontal: 15,
+    borderRadius: 5,
   },
 })
